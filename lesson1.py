91667cb309913d4de2f74dfd366a59894d592d4f
horacebeta <horacelsh@outlook.com>
2022-06-24 11:41:00 +0800

    init

--- lesson1.py
+++ lesson1.py
@@ -1,1 +1,14 @@
-print('hello world!')
+####輸入輸出###
+print('hello,world')
+print('How are you?','I\'m fine, thank you.')# 逗號代表space; \用作轉義
+    #input
+name = input()
+print('hello,', name)
+    #python采用縮進語法
+a =100
+if a >= 0:
+    print(a)
+else:
+    print(-a)
+
+
